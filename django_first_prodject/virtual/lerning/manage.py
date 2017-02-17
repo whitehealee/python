c5ee7953e79a777ad7fa6e145cfdd1db37eea4da
Березнюк Андрей <C:\Users\Berezniuk.a\AppData\Roaming\The Bat!>
2017-02-17 16:55:13 +0200

    1

--- django_first_prodject/virtual/lerning/manage.py
+++ django_first_prodject/virtual/lerning/manage.py
@@ -20,3 +20,15 @@ if __name__ == "__main__":
             )
         raise
     execute_from_command_line(sys.argv)
+	
+	
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'mysite',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
